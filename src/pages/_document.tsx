e594706f6c93819dc835b053e56c75622c81e9f7
ungarmichael <ungarmichael.mail@gmail.com>
2022-05-07 18:16:44 +0200

    update document for sc-rendering

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,36 +1,43 @@
-import Document, {
-  DocumentContext,
-  DocumentInitialProps,
-  Head,
-} from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
-export default class WbytDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }: any): any {
     const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
+    const page = renderPage(
+      (App: any) => (props: any) => sheet.collectStyles(<App {...props} />)
+    );
 
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: [
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>,
-        ],
-      };
-    } finally {
-      sheet.seal();
-    }
+    const styleTags = sheet.getStyleElement();
+
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap"
+            rel="stylesheet"
+          ></link>
+          {/* styled-components styles */}
+          {this.props.styles}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
