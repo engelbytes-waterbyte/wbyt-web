de5f485d8fd5d643b8fd63c4594999334bb97045
Michael Ungar <michael.ungar03@gmail.com>
2022-08-20 19:52:04 +0200

    fix: contact button with no function

--- src/components/aboutSections/s5-contact.tsx
+++ src/components/aboutSections/s5-contact.tsx
@@ -15,13 +15,15 @@ const S5Contact = () => {
       <div className="flex flex-col relative justify-center items-center mt-20 mb-40">
         <h3 className="mb-4 font-bold">contact@waterbyte.studio</h3>
         <RefArr5 />
-        <button
-          type="button"
-          className="flex text-white items-center bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-4"
-        >
-          <Mail className="mr-2" />
-          Send us a Message
-        </button>
+        <a href="mailto:michael.ungar03+wbyt@gmail.com">
+          <button
+            type="button"
+            className="flex text-white items-center bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-4"
+          >
+            <Mail className="mr-2" />
+            Send us a Message
+          </button>
+        </a>
       </div>
     </AboutItemLayout>
   );
