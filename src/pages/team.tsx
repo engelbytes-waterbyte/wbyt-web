fa030bdd4649520cd79d9c2bf7f49aae5e81c591
Michael Ungar <michael.ungar03@gmail.com>
2022-08-20 19:14:16 +0200

    feat: add team page

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -1,57 +1,66 @@
 import { NextPage } from "next";
+import Image from "next/image";
+
+import { BrandGit, BrandGithub, BrandInstagram } from "tabler-icons-react";
 
 const Team: NextPage = () => {
   return (
-    <div>
-      <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-          <img
-            class="rounded-t-lg"
-            src="/docs/images/blog/image-1.jpg"
-            alt=""
-          />
-        </a>
-        <div class="p-5">
-          <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
+    <div className="flex flex-col items-center md:flex-wrap md:flex-row">
+      <TeamItem
+        name="ungarmichael"
+        description="Hi, I'm Michael. I'm a developer and I love to build things."
+        instagram="https://www.instagram.com/ungrmichael"
+        github="htttps://github.com/ungarmichael"
+        imageUrl="https://images.obi.at/product/DE/1500x1500/218183_1.jpg"
+        role="Leiter"
+      />
+    </div>
+  );
+};
+
+interface TeamItemProps {
+  name: string;
+  role: string;
+  imageUrl: string;
+  description: string;
+  github: string;
+  instagram: string;
+}
+
+const TeamItem = ({
+  name,
+  role,
+  imageUrl,
+  description,
+  github,
+  instagram,
+}: TeamItemProps) => {
+  return (
+    <div className="max-w-sm m-8 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img className="rounded-t-md" src={imageUrl} alt="büd" />
+      </a>
+      <div className="p-5">
+        <h5 className="mt-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          @{name}
+        </h5>
+        <h6 className="mb-2 text-sm font-bold tracking-tight text-gray-500 dark:text-white">
+          {role}
+        </h6>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {description}
+        </p>
+        <div className="flex">
+          <a href={github}>
+            <BrandGithub className="mr-2" />
           </a>
-          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #photography
-          </span>
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #travel
-          </span>
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #winter
-          </span>
-          <a
-            href="#"
-            class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Read more
-            <svg
-              aria-hidden="true"
-              class="ml-2 -mr-1 w-4 h-4"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
+          <a href={instagram}>
+            <BrandInstagram className="mr-2" />
           </a>
         </div>
       </div>
     </div>
   );
 };
+
 export default Team;
