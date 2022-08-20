144a7d86da3c544c02832bc559e8810ebad272c2
Michael Ungar <michael.ungar03@gmail.com>
2022-08-20 19:16:31 +0200

    feat: add projects page content

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -34,16 +34,36 @@ const AboutSection = styled.section`
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <div class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Noteworthy technology acquisitions 2021
-        </h5>
-        <p class="font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
-        </p>
-      </div>
+    <div className="flex flex-col items-center md:flex-wrap md:flex-row ">
+      <Project
+        title="Jamboree"
+        description="Jamboree is a platform where event-organizers and consumers find together."
+      />
+      <Project
+        title="Heed"
+        description="Heed is an application for forecasting breakdowns of motorcycle components."
+      />
+      <Project
+        title="Charge"
+        description="Charge is a webapplication for calculating prices of rental properties."
+      />
+      <Project
+        title="Deets"
+        description="Deets is a system for getting further product information through QR codes."
+      />
+    </div>
+  );
+};
+
+const Project = (props: any) => {
+  return (
+    <div className="m-8 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {props.title}
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        {props.description}
+      </p>
     </div>
   );
 };
