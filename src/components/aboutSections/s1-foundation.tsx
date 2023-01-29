aad09283fe4da61fbaabb0c4baebf123ac726082
Michael Ungar <michael.ungar03@gmail.com>
2023-01-29 22:58:28 +0100

    ok

--- src/components/aboutSections/s1-foundation.tsx
+++ src/components/aboutSections/s1-foundation.tsx
@@ -26,7 +26,21 @@ const S1Foundation = () => {
         largeText="Die Entwicklungszentrale der hellsten Köpfe des Mittelmeerraums."
         content="Aus jahrelanger Erfahrung in der selben Branche kristallisierte sich aus einem Gemenge von Entwicklern die dem Waterbyte-Ideal einst am ehesten entsprachen eine Gruppierung von unglaublicher Einzigartigkeit. Wir haben es uns zur Aufgabe gemacht, die Gesinnung der altmodischen Projektentwicklungs-Techniken neu zu erfinden, um somit deren Geist weiterzuführen."
       >
-        <Rive src="/hammeranimation.riv" />
+        <div
+          className="h-32 w-32 left-1/2 relative -translate-x-1/2 mb-4  before:absolute
+	before:w-full
+	before:h-full
+	before:-z-10
+	before:bg-gradient-to-b
+	before:from-white
+	before:to-blue-300
+	before:left-0
+	before:top-0
+	before:rounded-full
+	before:blur-[30px] from-white/60 to-blue-300/60"
+        >
+          <Rive src="/hammeranimation.riv" />
+        </div>
         <DescriptionContainer>
           <RefArr1 />
           <h2 className="font-bold mb-2 mt-3">
@@ -34,8 +48,8 @@ const S1Foundation = () => {
           </h2>
           <DescriptionText>
             Lehrer Bert verließ die Schule und direkt im Schuljahr darauf führte
-            Lehrer Gertsch einst aufgrund eines Projektes eine erhöhte
-            Dichte von Waterbyte-DNA-Trägern herbei, wodurch es zum Urknall der
+            Lehrer Gertsch einst aufgrund eines Projektes eine erhöhte Dichte
+            von Waterbyte-DNA-Trägern herbei, wodurch es zum Urknall der
             Waterbyte-Geschichte kam.
           </DescriptionText>
         </DescriptionContainer>
